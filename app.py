c26c84b6b7ad6e17565210140838071ce8506fc8
shin0731 <86943851+shin0731@users.noreply.github.com>
2021-07-07 15:54:30 +0900

    名前と年齢を代入させました

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 # Flaskからimportしてflaskを使えるようにする
-from flask import Flask
+from flask import Flask,render_template
+from werkzeug.utils import redirect
 
 # app っていう名前でFlaskアプリをつくっていくよ～みたいな
 app = Flask(__name__)
@@ -22,6 +23,13 @@ def helloWorld():
 def greet(name):
     return name + "さん、はろー！！"
 
+# 新しいルートを作成
+@app.route("/test") 
+def test():
+    name = "羊仙人"
+    age = 100
+    return render_template("index.html",name = name ,age = age)
+
 
 # @app.route("/template")
 # def template():
@@ -29,10 +37,8 @@ def greet(name):
 #     return render_template("index.html", name = py_name)
 
 
-     
-
 
 
 if __name__ == "__main__":
-# Flask が持っている開発用サーバーを、実行します。
+# Flask が持っている開発用サーバーを、実行します。必ず最後に！
     app.run(debug=True)
